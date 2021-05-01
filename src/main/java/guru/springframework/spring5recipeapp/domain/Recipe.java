c7ad2e4dddc6be0d38a8cb2568cf1f0b81e2c275
shenoy <rajeshtshenoy@gmail.com>
2021-05-01 09:56:50 +0100

    added Enum difficulty

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -24,6 +24,17 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Long getId() {
         return id;
     }
@@ -102,5 +113,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
